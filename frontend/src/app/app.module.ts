7f477aa48f97a0b93e4b0520dd29a0ecabfe71b1
Stefan Fuchs <stefan@fuchs.pub>
2025-02-14 17:42:27 +0100

    wip

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiService } from './svc/api.service';
 import { SharedDataService } from './svc/shared-data.service';
+import { NavbarComponent } from './comp/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/comp/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kb-navbar',
+  standalone: false,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
