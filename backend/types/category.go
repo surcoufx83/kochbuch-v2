81eead246164f0926f97aaf7a01141a2c1cd5857
Stefan Fuchs <stefan@fuchs.pub>
2025-02-15 18:26:42 +0100

    wip

--- backend/types/category.go
+++ backend/types/category.go
@@ -4,16 +4,16 @@ import "time"
 
 // Category represents a category in the database
 type Category struct {
-	CategoryId   uint16         `db:"catid" json:"id"`
-	CategoryName string         `db:"catname" json:"name"`
-	CategoryIcon string         `db:"caticon" json:"icon"`
-	Modified     time.Time      `db:"catmodified" json:"modified"`
+	CategoryId   uint16         `json:"id"`
+	CategoryName string         `json:"name"`
+	CategoryIcon string         `json:"icon"`
+	Modified     time.Time      `json:"modified"`
 	Items        []CategoryItem `json:"items"`
 }
 
 type CategoryItem struct {
-	CategoryItemId   uint16    `db:"itemid" json:"id"`
-	CategoryItemName string    `db:"itemname" json:"name"`
-	CategoryItemIcon string    `db:"itemicon" json:"icon"`
-	Modified         time.Time `db:"itemmodified" json:"modified"`
+	CategoryItemId   uint16    `json:"id"`
+	CategoryItemName string    `json:"name"`
+	CategoryItemIcon string    `json:"icon"`
+	Modified         time.Time `json:"modified"`
 }
